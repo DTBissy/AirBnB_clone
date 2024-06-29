2f70a1a507fd4b955e49929b60d1cb7a9c157cc5
AllenKeelig <alallen.keeling@atlasschool.com>
2024-06-29 14:22:18 -0500

    merging

--- models/base_model.py
+++ models/base_model.py
@@ -20,8 +20,8 @@ class BaseModel:
         """Instantiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.strptime(kwargs.get('created_at', datetime.utcnow().isoformat()), '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(kwargs.get('updated_at', datetime.utcnow().isoformat()), '%Y-%m-%dT%H:%M:%S.%f')
         else:
             self.id = kwargs.get('id', str(uuid.uuid4()))
             self.created_at = datetime.strptime(kwargs.get('created_at', datetime.utcnow().isoformat()), '%Y-%m-%dT%H:%M:%S.%f')
