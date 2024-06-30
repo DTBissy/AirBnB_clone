7388a8d2e5b87e034d08c725935179a3c747f7a6
AllenKeelig <alallen.keeling@atlasschool.com>
2024-06-30 12:40:52 -0500

    state 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.city import City
+import models
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+
+    @property
+    def cities(self):
+        """Getter attribute cities that returns the list of City instances
+        with state_id equals to the current State.id for FileStorage relationship."""
+        if models.storage_type == 'db':
+            return self.cities
+        else:
+            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
