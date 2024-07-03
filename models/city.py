ffa6ddb3a4ba922ec6afb185493df8c043e9e480
Dtbissy <isaactheedwards@gmail.com>
2024-07-02 22:20:31 -0500

    Something that qorks sort of

--- models/city.py
+++ models/city.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from sqlalchemy import Column, String, ForeignKey
-from models.base_model import BaseModel, Base
+"""This module defines a class City"""
 
+from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """This class represents a city"""
     __tablename__ = 'cities'
-    
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    # state = relationship('State', backref='cities')
--- models/user.py
+++ models/user.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+""" THis module defines a class User"""
+
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
-
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
-    """User"""
+    """User class definition."""
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+
+    places = relationship("Place", backref="owner_user", cascade="all, delete-orphan")
