8f1de4abe32568d4cc889a4ba50451559c4a95e5
Dtbissy <isaactheedwards@gmail.com>
2024-07-06 15:32:09 -0500

    added some missing datatype

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -32,7 +32,7 @@ def number(n):
     return "{:d} is a number".format(n)
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
+def number_template(n: int):
     return render_template('5-number.html', n=n)
 
 
