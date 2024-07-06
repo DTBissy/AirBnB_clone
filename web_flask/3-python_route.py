9b8227fe36568521467d6bf8138be2f78b6492ce
Dtbissy <isaactheedwards@gmail.com>
2024-07-06 14:56:06 -0500

    AHAHA we did it boys

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -20,6 +20,8 @@ def hbnh():
 def C(text):
     return "C {}".format(text.replace("_"," "))
 
+
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>',strict_slashes=False)
 def python(text="is cool"):
     return "Python {}".format(text.replace("_", " "))
